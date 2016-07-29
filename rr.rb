43cee78aec42a2bb500d0a4fa8eda65b90efd2ea
Charuth <charuthsudhakaran@gmail.com>
2016-07-29 13:58:31 +0530

    Corrected_ Program

---
+++ rr.rb
@@ -0,0 +1,7 @@
+require 'sinatra'
+set :port, 5000
+get '/' do
+  query_result = {"Find" => 'Find Query', "Search" => 'Searchquery'}
+  result = query_result[params['query']]
+  result || 'default query'
+  end
